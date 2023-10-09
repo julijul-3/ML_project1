4e08a4ad4d24e0b6782d967f04c2dd22a0ccc752
julijul-3 <127526202+julijul-3@users.noreply.github.com>
2023-10-09 15:35:27 +0200

    juste une mise en page

---
+++ utils/utils.py
@@ -0,0 +1,15 @@
+import numpy as np 
+
+def mean_squared_error_gd(y, tx, initial_w, max_iters, gamma):
+    """Linear regression using gradient descent
+    Args: 
+        y: numpy array of shape (N,), N is the number of samples.
+        tx: numpy array of shape (N,D), D is the number of features.
+        initial_w: initial weight vector
+        max_iters: int, number of steps to run
+        gamma: step-size
+    Retruns:
+        w: last weight
+        loss:
+    """
+    return None
